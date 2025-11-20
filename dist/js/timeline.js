aa26dc8a893fe01b2d7ec2ac260eb15049db8eaa
goncalves1-create <goncalves1@edu.ulisboa.pt>
2025-11-20 00:52:05 +0000

    Describe your changes

--- dist/js/timeline.js
+++ dist/js/timeline.js
@@ -13939,15 +13939,20 @@ class TimeNav {
 
         // Size Markers
         this._assignRowsToMarkers();
-
+        
         // Size swipable area
-        this._el.slider_background.style.width = this.timescale.getPixelWidth() + this.options.width + "px";
-        this._el.slider_background.style.left = -(this.options.width / 2) + "px";
-        this._el.slider.style.width = this.timescale.getPixelWidth() + this.options.width + "px";
+        this._el.slider_background.style.width = this.timescale.getPixelWidth() + "px";
+        this._el.slider_background.style.left = "0px";
+        this._el.slider.style.width = this.timescale.getPixelWidth() + "px";
 
         // Update Swipable constraint
-        this._swipable.updateConstraint({ top: false, bottom: false, left: (this.options.width / 2), right: -(this.timescale.getPixelWidth() - (this.options.width / 2)) });
-
+        this._swipable.updateConstraint({ 
+        top: false, 
+        bottom: false, 
+        left: 0, 
+        right: -this.timescale.getPixelWidth() + this.options.width 
+        });
+        
         if (reposition_markers) {
             this._drawTimeline()
         }
