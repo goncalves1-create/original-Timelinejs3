5f7de8d2453f7547e8c5967398e98c55c59cf0bb
goncalves1-create <goncalves1@edu.ulisboa.pt>
2025-11-21 16:47:05 +0000

    Update TimeNav.js

--- src/js/timenav/TimeNav.js
+++ src/js/timenav/TimeNav.js
@@ -142,7 +142,7 @@ export class TimeNav {
     /*	Update Display
     ================================================== */
     updateDisplay(width, height, animate) {
-        console.log("=== updateDisplay DEBUG ===");
+        console.log("=== updateDisplay DEBUG 1 ===");
     console.log("options.width1:", this.options.width);
     console.log("options.height1:", this.options.height);
     console.log("timelineWidth:", this.timescale.getPixelWidth());
