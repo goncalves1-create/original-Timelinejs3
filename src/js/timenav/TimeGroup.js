de9cc6ab6e61173d5a62151a7b331eb4cf7d2b7f
goncalves1-create <goncalves1@edu.ulisboa.pt>
2025-11-21 17:19:29 +0000

    Update TimeGroup.js

--- src/js/timenav/TimeGroup.js
+++ src/js/timenav/TimeGroup.js
@@ -51,13 +51,14 @@ export class TimeGroup {
 		
 	}
 	
-	setRowPosition(n, h) {
-    console.log("TimeGroup setRowPosition called, current width:", this._el.container.style.width);
-    this.options.height = h * this.data.rows;
+	setRowPosition(n, h, max_levels = 1) {
+    // Calculate height based on levels instead of just rows
+    var level_height = h; // Height per level
+    var total_height = level_height * max_levels;
+    
+    this.options.height = total_height;
     this.setPosition({top:n});
     this._el.container.style.height = this.options.height + "px";
-    this._el.container.style.left = "0px";
-    // REMOVE the width calculation - let TimeNav.js set the width
 }
 	
 	setAlternateRowColor(alternate, hide) {
@@ -82,16 +83,14 @@ export class TimeGroup {
 	
 	/*	Private Methods
 	================================================== */
-	_initLayout() {
-    // Create Layout
-    this._el.container = DOM.create("div", "tl-timegroup");
-    
-    // MAKE GROUP NON-INTERACTIVE
-    this._el.container.style.pointerEvents = "none";
-    
-    this._el.message = DOM.create("div", "tl-timegroup-message", this._el.container);
-    this._el.message.innerHTML = this.data.label;
-    }
+	_initLayout () {
+		
+		// Create Layout
+		this._el.message = DOM.create("div", "tl-timegroup-message", this._el.container);
+		this._el.message.innerHTML = this.data.label;
+		
+		
+	}
 	
 	_initEvents () {
 		DOMEvent.addListener(this._el.container, 'click', this._onMouseClick, this);
